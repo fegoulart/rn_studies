8db1852471a336eb0b98c7e927949e306acb3442
Fernando Luiz Goulart <fernando.goulart@olxbr.com>
2020-01-30 12:39:09 -0300

    WIP: section2

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText ] = useState('Open up App.js to start working on your app!')
-  return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress ={()=> setOutputText('The text changed!')} />
-    </View>
-  );
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    }
+    const addGoalHandler = () => {
+        console.log(enteredGoal);
+        //setCourseGoals([...courseGoals, enteredGoal]);
+        setCourseGoals(currentGoals => {
+            return [...courseGoals, enteredGoal]
+        })
+    }
+    return (
+        <View style={styles.abacaxi}>
+            <View style={styles.salsixa}>
+                <TextInput
+                    placeholder='Course Goal'
+                    style={styles.batata}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
+                />
+                <Button title="Add" onPress={addGoalHandler}/>
+            </View>
+            <View>
+                {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    abacaxi: {
+        padding: 50,
+    },
+    salsixa: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    batata: {
+        borderColor: 'black',
+        borderWidth: 1,
+        width: '80%'
+    }
 });
